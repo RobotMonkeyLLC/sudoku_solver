a90b9e3fa3220ff4efed28d7506e67e310ae6fce
epinedo <epinedo@robotmonkeyllc.com>
2024-08-07 23:20:36 -0400

    more tracing

--- sudoku-solver/src/components/Cell.jsx
+++ sudoku-solver/src/components/Cell.jsx
@@ -4,7 +4,7 @@ import CellGroup from "./CellGroup.jsx";
 
 export default function Cell({value, coor, shouldStop = false}) {
     const { takingNotes, notes, puzzle, selected, setSelected } = useContext(SudokuContext);
-    const {g, i, j} = coor;
+    const {g, i, j, n} = coor;
 
     const handleClick = () => {
       /*   if (takingNotes) {
@@ -15,15 +15,15 @@ export default function Cell({value, coor, shouldStop = false}) {
         setSelected({...coor});
     }
     
-    const isNoteTrue = notes[g][i][j][value];
-    const isPuzzle = (j !== false) && (puzzle[g][i][j] !== 0); // needs rework
+    const isPuzzle = (puzzle[g][i][j] !== 0); // needs rework
+    //const isNote = n;
+    const isNoteTrue = n && notes[g][i][j][value];
     const isSelected = selected && selected.g === g && selected.i === i && selected.j === j;
-    const isNote = (isPuzzle == false) && (takingNotes);
     //const puzzleClass = (puzzle[g][i][j] === 0 ? 'cursor-pointer fs-2' : 'fs-1 text-dark-emphasis fw-bold');
-    const noteClass = isNote ? ' note-cell ' : false;
-    const baseClass = `${noteClass || 'cell '} d-flex flex-wrap p-0 border border-black align-content-center justify-content-center`;
-    const puzzleClass = isPuzzle ? ' fs-1 text-dark-emphasis fw-bold ' : ` cursor-pointer ${isNote ? '' : 'fs-2'} `;
-    const selectedClass = isSelected ? ` bg-${isNoteTrue ? "dark" : "white"} opacity-${isNote ? 50 : 25} `:` bg-white bg-opacity-${50} `;
+    //const noteClass = isNote ? ' note-cell ' : false;
+    const baseClass = `${n ? ' note-cell ' : 'cell '} d-flex flex-wrap p-0 border border-black align-content-center justify-content-center`;
+    const puzzleClass = isPuzzle ? ' fs-1 text-dark-emphasis fw-bold ' : ` cursor-pointer fs-${n ? 4 : 2} `;
+    const selectedClass = isSelected ? ` bg-${isNoteTrue ? "dark" : "white"} opacity-${n ? 50 : 25} `:` bg-white bg-opacity-${50} `;
 
     return (
         <div className={baseClass + puzzleClass + ' ' + selectedClass}
--- sudoku-solver/src/components/CellGroup.jsx
+++ sudoku-solver/src/components/CellGroup.jsx
@@ -6,9 +6,9 @@ export default function CellGroup({group, g, shouldStop = false}) {
             group.map((x, i) => (
                 
                         x.length && x.map((y, j) => (
-                            <Cell key={j} value={y} coor={{g:g, i:i, j:j}} shouldStop></Cell>
+                            <Cell key={j} value={y} coor={{g:g, i:i, j:j, n:false}} shouldStop></Cell>
                         )) ||
-                        <Cell key={i+'_2'} value={i+1} coor={{g:g.g, i:g.i, j:i}} shouldStop={false}></Cell>
+                        <Cell key={i+'2'} value={i} coor={{g:g.g, i:g.i, j:i, n:true}} shouldStop={false}></Cell>
                         
                     
             ))
